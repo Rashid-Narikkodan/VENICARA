a8f2737149ba351a84ff8c994d158d67acf4f729
Rashid <rashidnarikkodan@gmail.com>
2025-10-06 09:20:52 +0530

    razorpay error handler updated

--- controllers/user/checkoutController.js
+++ controllers/user/checkoutController.js
@@ -457,8 +457,8 @@ const handlePlaceOrder = async (req, res) => {
     if (paymentMethod === "RAZORPAY") {
       const MAX_AMOUNT = 50000000; // In paise, for ₹5,00,000
 if (finalAmount > MAX_AMOUNT) {
-  return res.status(400).json({
-    error: "Amount exceeds maximum allowed in Razorpay Test Mode."
+  return res.status(400).json({status: false,
+    message: "Amount exceeds maximum allowed in Razorpay Test Mode."
   });
 }
 
